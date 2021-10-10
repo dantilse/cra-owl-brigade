59c002b7177ffd346bb435603e608246e4538f2f
Daniel Ilse <dantilse@gmail.com>
2021-10-09 20:38:21 -0500

    chore: add routes pages and auth to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import PageOne from "./pages/page-1";
+import PageTwo from "./pages/page-2";
+import Nav from "./components/Nav";
+import SecureRoute from "./components/SecureRoute";
+import UnsecureRoute from "./components/UnsecureRoute";
+import { AuthContextProvider } from "./firebase";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <SecureRoute path="/" exact component={Home} />
+            <UnsecureRoute path="/login" exact component={Login} />
+            <SecureRoute path="/page-1" exact component={PageOne} />
+            <SecureRoute path="/page-2" exact component={PageTwo} />
+          </Switch>
+        </div>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
