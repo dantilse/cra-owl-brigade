c9decdc104a98d0413e5f626839cb1d6b75e85a7
Daniel Ilse <dantilse@gmail.com>
2021-10-11 13:32:18 -0500

    chore: create loading page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
 import CityPage from "./CityPage";
 import LandingPage from "./LandingPage";
+import LoadingPage from "./LoadingPage";
 import LoginPage from "./LoginPage";
 import PageNotFound from "./404";
 import SuggestionsPage from "./SuggestionsPage";
 
-export { CityPage, LandingPage, LoginPage, PageNotFound, SuggestionsPage };
+export {
+  CityPage,
+  LandingPage,
+  LoadingPage,
+  LoginPage,
+  PageNotFound,
+  SuggestionsPage,
+};
