19386a11c8c175a2e483cbbb392828719d268c14
Daniel Ilse <dantilse@gmail.com>
2021-10-25 21:16:40 -0500

    chore: add new pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
+import CalendarPage from "./CalendarPage";
 import CityPage from "./CityPage";
 import LandingPage from "./LandingPage";
 import LoadingPage from "./LoadingPage";
 import LoginPage from "./LoginPage";
 import PageNotFound from "./404";
+import ProfilePage from "./ProfilePage";
 import SuggestionsPage from "./SuggestionsPage";
 
 export {
+  CalendarPage,
   CityPage,
   LandingPage,
   LoadingPage,
   LoginPage,
   PageNotFound,
+  ProfilePage,
   SuggestionsPage,
 };
